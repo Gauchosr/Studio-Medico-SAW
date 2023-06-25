e7452157d40412f3975e3fe8cceb8b170d498f12
Gauchosr <36105243+Gauchosr@users.noreply.github.com>
2023-06-25 17:39:07 +0200

    Delete app directory

--- app/modelli/medico.model.ts
+++
@@ -1,9 +0,0 @@
-export class Medico {
-  constructor(
-    public localId: string, //su questo indice devo fare l'aggiunta al DB
-    public idToken: string, //token di autenticazione
-    public _expDate: Date,
-    public nome: string,
-    public cognome: string
-  ){}
-}
--- app/modelli/paziente.model.ts
+++
@@ -1,9 +0,0 @@
-export class Paziente {
-  constructor(
-    public localId: string, //su questo indice devo fare l'aggiunta al DB
-    public idToken: string, //token di autenticazione
-    public _expDate: Date,
-    public nome: string,
-    public cognome: string
-  ){}
-}
